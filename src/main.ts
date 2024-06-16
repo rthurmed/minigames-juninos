41401b94a8eb670b4d37fc95b493d9e374665692
Arthur <rthurmedeiros@gmail.com>
2024-06-16 20:22:47 -0300

    Moved scene switcher into file

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { config } from "./config"
 import { makeScenePalhaco } from "./games/palhaco"
 import { makeSceneBurro } from "./games/burro"
 import { makeSceneOvo } from "./games/ovo"
+import { makeSceneSwitcher } from "./switcher"
 
 const k = startGame({
 	width: config.GAME_WIDTH,
@@ -22,33 +23,7 @@ k.loadSprite("clown", "sprites/clown-Sheet.png", {
 k.scene("palhaco", makeScenePalhaco(k))
 k.scene("burro", makeSceneBurro(k))
 k.scene("ovo", makeSceneOvo(k))
+k.scene("switcher", makeSceneSwitcher(k))
 
-k.scene("main", () => {
-	const scenes = [
-		"palhaco",
-		"burro",
-		"ovo"
-	]
-	
-	const sceneSwitcher = k.add([
-		k.pos(16, k.height() - 16),
-		k.anchor("botleft"),
-		k.z(20)
-	])
-	
-	for (let i = 0; i < scenes.length; i++) {
-		const scene = scenes[i];
-		const button = sceneSwitcher.add([
-			k.pos(i * (16 + 4), -16),
-			k.rect(16, 16),
-			k.color(k.WHITE),
-			k.area()
-		])
-		button.onClick(() => {
-			k.go(scene)
-		})
-	}
-})
-
-// k.go("main")
+// k.go("switcher")
 k.go("palhaco")
---
+++ src/switcher.ts
@@ -0,0 +1,28 @@
+import { KaboomCtx } from "kaplay";
+
+export const makeSceneSwitcher = (k: KaboomCtx) => () => {
+	const scenes = [
+		"palhaco",
+		"burro",
+		"ovo"
+	]
+	
+	const sceneSwitcher = k.add([
+		k.pos(16, k.height() - 16),
+		k.anchor("botleft"),
+		k.z(20)
+	])
+	
+	for (let i = 0; i < scenes.length; i++) {
+		const scene = scenes[i];
+		const button = sceneSwitcher.add([
+			k.pos(i * (16 + 4), -16),
+			k.rect(16, 16),
+			k.color(k.WHITE),
+			k.area()
+		])
+		button.onClick(() => {
+			k.go(scene)
+		})
+	}
+}
